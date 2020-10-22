401aeefaaea65438bf149afa5931bf61236998a3
pierlucamario_dussich <pierluca.dussich@gmail.com>
2020-10-22 17:40:12 +0200

    D4casino| start

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// arrey con tutte le email del database
+
+var users = [ "aaaaa@hotmail.it", "bbbb@libero.it", "ccccc@gmail.com", "dddddd@virgilio.it"];
+
+// collego tutti gli id
+var btnVerificaEmail = document.getElementById("verifica-email");
+var message = document.getElementById("message");
+var findEmail = document.getElementById("find-email")
+
+btnVerificaEmail.addEventListener("click",
+function (){
+
+    find = findEmail.value;
+
+    for (var i=0; i < users.length ; i++) {
+
+      var notFind = false ;
+      if
+
+}
+
+ })
+
+for (var i=0; i < users.length ; i++) {
+
+}
